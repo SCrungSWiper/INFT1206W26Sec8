b22c48cd4ece68929ba659d3c139d9b5dffd56ed
SCrungSWiper <gcrash250@gmail.com>
2026-03-20 14:22:50 -0400

    added main story elements

--- Assignment4/Part1/generator.js
+++ Assignment4/Part1/generator.js
@@ -26,7 +26,11 @@ function returnRandomStoryString() {
 }
 
 function returnRandomStoryString() {
- 
+    let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} 
+    went for a walk. When they got to ${randomPlace}, 
+    they stared in horror for a few moments, then ${randomEvent}. 
+    Bob saw the whole thing, but was not surprised — ${randomCharacter} 
+    weighs 300 pounds, and it was a hot day.`;
     return storyText;
 }
 
