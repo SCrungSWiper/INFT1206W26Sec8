9d27c56d16e8223f37555a818c024a733e6a4b4d
SCrungSWiper <gcrash250@gmail.com>
2026-03-20 14:21:51 -0400

    added calculations for weight and temp

--- Assignment4/Part1/generator.js
+++ Assignment4/Part1/generator.js
@@ -5,30 +5,29 @@ const generateBtn = document.querySelector(".generate");
 const story = document.querySelector(".story");
 
 function randomValueFromArray(array) {
-  const random = Math.floor(Math.random() * array.length);
-  return array[random];
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
 }
 
 // Raw text strings
 const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
 
-const places = ["the soup kitchen", "Disneyland","the White House"]
+const places = ["the soup kitchen", "Disneyland", "the White House"]
 
-const events = ["spontaneously combusted", "melted into a puddle on the sidewalk","turned into a slug and slithered away"]
+const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"]
 
 
 
 
 function returnRandomStoryString() {
-  const randomCharacter = randomValueFromArray(characters);
-  const randomPlace = randomValueFromArray(places);
-  const randomEvent = randomValueFromArray(events);
+    const randomCharacter = randomValueFromArray(characters);
+    const randomPlace = randomValueFromArray(places);
+    const randomEvent = randomValueFromArray(events);
 }
 
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-
-  return storyText;
+ 
+    return storyText;
 }
 
 // Event listener and partial generate function definition
@@ -36,16 +35,18 @@ function returnRandomStoryString() {
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
-  if (customName.value !== "") {
-    const name = customName.value;
-  }
+    if (customName.value !== "") {
+        const name = customName.value;
+    }
 
-  if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
-  }
+    if (document.getElementById("uk").checked) {
+        const weight = `${Math.round(300 / 14)} stone`;
+        const temperature = `${Math.round((94 - 32) * (5 / 9))} Celsius`;
+        newStory = newStory.replace("300 pounds", weight);
+        newStory = newStory.replace("94 Fahrenheit", temperature);
+    }
 
-  // TODO: replace "" with the correct expression
-  story.textContent = "";
-  story.style.visibility = "visible";
+    // TODO: replace "" with the correct expression
+    story.textContent = "";
+    story.style.visibility = "visible";
 }
