e0af34288bd3765bfb666e69542956ad91f3bdbd
SCrungSWiper <gcrash250@gmail.com>
2026-04-04 17:30:54 -0400

    added mainline comments

--- Assignment4/Part2/javascipt.js
+++ Assignment4/Part2/javascipt.js
@@ -1,3 +1,9 @@
+
+//    Name: Lucas Gilmour
+//    Filename: javascript.js
+//    Current date: 2026-04-04
+//    description: Assignment 4, Image Gallery
+
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
