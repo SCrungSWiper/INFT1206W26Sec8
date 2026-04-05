3694f7130ae57c5f17541fa9ef5bc8ba3b0d4c9e
SCrungSWiper <gcrash250@gmail.com>
2026-04-05 15:23:47 -0400

    added images constant

--- Assignment4/Part2/javascipt.js
+++ Assignment4/Part2/javascipt.js
@@ -3,6 +3,13 @@
 //    Filename: javascript.js
 //    Current date: 2026-04-04
 //    description: Assignment 4, Image Gallery
+const images = [
+  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
 
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
