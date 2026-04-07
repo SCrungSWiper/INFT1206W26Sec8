0978f11f8b568ab6c4de6b7924d971da1437c45c
SCrungSWiper <gcrash250@gmail.com>
2026-04-06 20:27:58 -0400

    added mainline comments

--- Assignment4/Part3/main.js
+++ Assignment4/Part3/main.js
@@ -1,3 +1,8 @@
+//    Name: Lucas Gilmour
+//    Filename: main.js
+//    Current date: 2026-04-06
+//    description: Assignment 4, bouncing balls
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
