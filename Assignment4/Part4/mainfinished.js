c7f122a41e9af9f91da9a522b5d05df66e235e88
SCrungSWiper <gcrash250@gmail.com>
2026-04-10 15:25:29 -0400

    added evilcircle (unfixed bugs remain)

--- Assignment4/Part4/mainfinished.js
+++ Assignment4/Part4/mainfinished.js
@@ -18,7 +18,14 @@ function randomRGB() {
     return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
 }
 
-
+class Shape {
+    constructor(x, y, velX, velY) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+    }
+}
 
 class Ball {
     constructor(x, y, velX, velY, color, size) {
@@ -59,21 +66,46 @@ class Ball {
     }
 
     collisionDetect() {
-    for (const ball of balls) {
-      if (!(this === ball) && ball.exists) {
-        const dx = this.x - ball.x;
-        const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
+        for (const ball of balls) {
+            if (!(this === ball) && ball.exists) {
+                const dx = this.x - ball.x;
+                const dy = this.y - ball.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
+                if (distance < this.size + ball.size) {
+                    ball.color = this.color = randomRGB();
+                }
+            }
         }
-      }
     }
-  }
 
 }
 
+class evilCircle extends Shape {
+    constructor(x, y) {
+        super(x, y, 20, 20)
+        this.color = 'white'
+        this.size = 10;
+
+        window.addEventListener("keydown", (e) => {
+            switch (e.key) {
+                case "a":
+                    this.x -= this.velX;
+                    break;
+                case "d":
+                    this.x += this.velX;
+                    break;
+                case "w":
+                    this.y -= this.velY;
+                    break;
+                case "s":
+                    this.y += this.velY;
+                    break;
+            }
+        });
+    }
+
+}
 const testBall = new Ball(50, 100, 4, 4, "blue", 10);
 
 testBall.draw();
