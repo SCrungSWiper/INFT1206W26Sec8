2f3d5b38340e472f758878b1e57ca9a0967f2098
SCrungSWiper <gcrash250@gmail.com>
2026-04-10 15:07:13 -0400

    updated collision detection

--- Assignment4/Part4/mainfinished.js
+++ Assignment4/Part4/mainfinished.js
@@ -59,18 +59,18 @@ class Ball {
     }
 
     collisionDetect() {
-        for (const ball of balls) {
-            if (this !== ball) {
-                const dx = this.x - ball.x;
-                const dy = this.y - ball.y;
-                const distance = Math.sqrt(dx * dx + dy * dy);
+    for (const ball of balls) {
+      if (!(this === ball) && ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
 
-                if (distance < this.size + ball.size) {
-                    ball.color = this.color = randomRGB();
-                }
-            }
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
         }
+      }
     }
+  }
 
 }
 
